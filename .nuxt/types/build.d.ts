c4939ad98427d1b9a02b6b7b75bcbc91e5f9cc50
thanhnam43048 <thanhnam43048@gmail.com>
2025-04-22 15:00:21 +0700

    push

--- .nuxt/types/build.d.ts
+++ .nuxt/types/build.d.ts
@@ -12,7 +12,7 @@ declare module "#build/root-component.mjs";
 declare module "#build/plugins.server.mjs";
 declare module "#build/test-component-wrapper.mjs";
 declare module "#build/devtools/settings.mjs";
-declare module "#build/runtime.vue-devtools-client.osJMbwenlWVa4XRG124CosLLdc6M41uNUmDgupWdMwk.js";
+declare module "#build/runtime.vue-devtools-client.y76szGPNFp47OgbHth6s2yKHYp6h1_itt4dVShVptdI.js";
 declare module "#build/routes.mjs";
 declare module "#build/pages.mjs";
 declare module "#build/router.options.mjs";
