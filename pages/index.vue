a2e5efd05a924f9fc8154a0c540a1c95ee39082a
binhantt <doan44503@gmail.com>
2025-04-22 14:56:04 +0700

    feast : nabar

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <template>
+   <MyComponent />
   <div class="home-page flex flex-col justify-center items-center min-h-screen bg-gray-100">
     <h1 class="text-4xl font-bold text-gray-800 mb-4">Chào mừng đến trang chủ</h1>
     <p class="text-lg text-gray-600">Đây là trang chủ của dự án Nuxt.js của bạn.</p>
+    <!-- Sử dụng component đã import -->
+   
   </div>
 </template>
 
 <script setup>
-// Bạn có thể thêm logic JavaScript ở đây nếu cần
+// Import component
+import MyComponent from '../components/Layout/default.vue';
 </script>
 
 <style scoped>
